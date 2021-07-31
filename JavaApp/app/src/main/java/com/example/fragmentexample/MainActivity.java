bc8521ca1394cfcbe8eaec9993257ce465f4106e
saehyun <sh007100@naver.com>
2021-07-31 23:00:23 +0900

    [ADD] fgex MainActivity

--- JavaApp/app/src/main/java/com/example/fragmentexample/MainActivity.java
+++ JavaApp/app/src/main/java/com/example/fragmentexample/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.fragmentexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_1, btn_2, btn_3, btn_4
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment1 fragment1 = new Fragment1();
+            }
+        });
+
     }
 }
