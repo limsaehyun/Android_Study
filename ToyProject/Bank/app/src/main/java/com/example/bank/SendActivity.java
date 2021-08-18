e08ea754b2376aad5988b4b5d8f70e903ceb2070
limsaehyun <sh00710000@gmail.com>
2021-08-18 11:25:53 +0900

    Feat :: SeeActivity

---
+++ ToyProject/Bank/app/src/main/java/com/example/bank/SendActivity.java
@@ -0,0 +1,14 @@
+package com.example.bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
+    }
+}
