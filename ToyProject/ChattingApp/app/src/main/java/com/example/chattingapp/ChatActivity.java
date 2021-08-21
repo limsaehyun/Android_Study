2a09512723b1dbb42e66335173201fe09bdefd1c
limsaehyun <sh00710000@gmail.com>
2021-08-21 15:30:27 +0900

    Feat :: 채팅 페이지 생성

---
+++ ToyProject/ChattingApp/app/src/main/java/com/example/chattingapp/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.chattingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
