f4aff92ed0c20bae658cc97fcdd549b64f4df574
limsaehyun <sh00710000@gmail.com>
2021-08-27 22:28:25 +0900

    Delete :: ChatActivity 삭제

--- JavaApp/ChatApp/app/src/main/java/com/example/chatapp/MainActivity.java
+++ JavaApp/ChatApp/app/src/main/java/com/example/chatapp/MainActivity.java
@@ -2,27 +2,14 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_login;
-    Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_login = (Button) findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ChatActivity.class));
-            }
-        });
+        
     }
 }
