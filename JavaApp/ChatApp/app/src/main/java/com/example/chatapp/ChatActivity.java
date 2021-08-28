093e0681fe7decba2d7f7bb8a243a08e8c9beb52
saehyun <sh00710000@gmail.com>
2021-08-28 12:18:46 +0900

    Feat :: 리사이클러뷰와 어댑터 결합

--- JavaApp/ChatApp/app/src/main/java/com/example/chatapp/ChatActivity.java
+++ JavaApp/ChatApp/app/src/main/java/com/example/chatapp/ChatActivity.java
@@ -1,14 +1,31 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class ChatActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    MyAdapter mAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] myDataset = {"test1", "test2", "test3", "test4"};
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
