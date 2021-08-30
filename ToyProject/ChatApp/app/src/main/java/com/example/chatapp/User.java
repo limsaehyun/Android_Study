bebcfdddd744f1e3c7b58dd4000050c24527f67b
limsaehyun <sh00710000@gmail.com>
2021-08-30 19:06:10 +0900

    Feat :: 유저  데이터를 담아두는 데이터 클래스

---
+++ ToyProject/ChatApp/app/src/main/java/com/example/chatapp/User.java
@@ -0,0 +1,13 @@
+package com.example.chatapp;
+
+public class User {
+    String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
