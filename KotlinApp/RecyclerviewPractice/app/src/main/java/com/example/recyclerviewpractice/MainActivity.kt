59adb1ef294f1f908adee6c389b6344cd93cec64
limsaehyun <sh00710000@gmail.com>
2021-09-10 23:17:42 +0900

    Feat :: connect adapter

--- KotlinApp/RecyclerviewPractice/app/src/main/java/com/example/recyclerviewpractice/MainActivity.kt
+++ KotlinApp/RecyclerviewPractice/app/src/main/java/com/example/recyclerviewpractice/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.recyclerviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    var studentList = arrayListOf<Student>(
+        Student(1315, "임세현", "android"),
+        Student(1315, "임세현", "android"),
+        Student(1315, "임세현", "android")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvStudent: RecyclerView = findViewById(R.id.rvStudent)
+
+        val mAdapter = MainRvAdapter(this, studentList)
+        rvStudent.adapter = mAdapter
+
+        val lm = LinearLayoutManager(this)
+        rvStudent.layoutManager = lm
+        rvStudent.setHasFixedSize(true)
     }
 }
