4161fa43fa4b4b31cafc9f91abd932366fc009a0
saehyun <sh00710000@gmail.com>
2021-09-16 22:17:21 +0900

    Feat :: Create Project

---
+++ KotlinApp/FingerprintAuthentication/app/src/main/java/com/example/fingerprintauthentication/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.fingerprintauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
