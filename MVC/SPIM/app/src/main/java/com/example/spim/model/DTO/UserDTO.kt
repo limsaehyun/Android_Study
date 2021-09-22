fe75dc57d066986c18b11d34164d52b393056ea6
saehyun <sh00710000@gmail.com>
2021-09-22 14:50:57 +0900

    Feat :: user DTO

--- MVC/SPIM/app/src/main/java/com/example/spim/model/DTO/UserDTO.kt
+++ MVC/SPIM/app/src/main/java/com/example/spim/model/DTO/UserDTO.kt
@@ -1,3 +1,3 @@
 package com.example.spim.model.DTO
 
-data class UserDTO (var name: String, var grade: String, var Class: String, var number: String)
+class UserDTO (var name: String, var grade: String, var Class: String, var number: String)
