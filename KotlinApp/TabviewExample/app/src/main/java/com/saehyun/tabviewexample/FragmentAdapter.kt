a9a948defe2208fad60ade6b0919aa59bfb8ae03
limsaehyun <sh00710000@gmail.com>
2021-09-25 20:09:08 +0900

    Feat :: FragmentAdapter

---
+++ KotlinApp/TabviewExample/app/src/main/java/com/saehyun/tabviewexample/FragmentAdapter.kt
@@ -0,0 +1,35 @@
+package com.saehyun.tabviewexample
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.saehyun.tabviewexample.BlankFragment
+import com.saehyun.tabviewexample.BlankFragment2
+import com.saehyun.tabviewexample.BlankFragment3
+
+class FragmentAdapter (fm : FragmentManager): FragmentPagerAdapter(fm) {
+    //position 에 따라 원하는 Fragment로 이동시키기
+    override fun getItem(position: Int): Fragment {
+        val fragment = when (position) {
+            0 -> BlankFragment().newInstant()
+            1 -> BlankFragment2().newInstant()
+            2 -> BlankFragment3().newInstant()
+            else -> BlankFragment().newInstant()
+        }
+        return fragment
+    }
+
+    //tab의 개수만큼 return
+    override fun getCount(): Int = 3
+
+    //tab의 이름 fragment마다 바꾸게 하기
+    override fun getPageTitle(position: Int): CharSequence? {
+        val title = when (position) {
+            0 -> "0ne"
+            1 -> "Two"
+            2 -> "Three"
+            else -> "main"
+        }
+        return title
+    }
+}
