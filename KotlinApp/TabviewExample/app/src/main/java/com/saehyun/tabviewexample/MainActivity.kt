00b5f24764b6095cc79c1394f5cb4ea3941e1b02
limsaehyun <sh00710000@gmail.com>
2021-09-25 20:09:48 +0900

    Feat :: tabLayout setup

---
+++ KotlinApp/TabviewExample/app/src/main/java/com/saehyun/tabviewexample/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.saehyun.tabviewexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.saehyun.tabviewexample.FragmentAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pagerAdapter = FragmentAdapter(supportFragmentManager)
+        val pager = findViewById<ViewPager>(R.id.viewPager)
+        pager.adapter = pagerAdapter
+        val tab = findViewById<TabLayout>(R.id.tab)
+        tab.setupWithViewPager(pager)
+    }
+}
