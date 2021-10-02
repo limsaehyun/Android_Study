5de3a7e07311dd71c0ad5e75c093dd4342203bf5
saehyun <sh00710000@gmail.com>
2021-10-02 16:00:55 +0900

    Feat :: Synchro example

---
+++ SynchronousAndAsynchronous/SynchroExample/src/Synchro.java
@@ -0,0 +1,21 @@
+public class Synchro {
+    public static void main(String[] args) {
+
+        method1();
+        method2();
+        method3();
+
+    }
+
+    private static void method1() {
+        System.out.println("method1");
+    }
+
+    private static void method2() {
+        System.out.println("method2");
+    }
+
+    private static void method3() {
+        System.out.println("method3");
+    }
+}
