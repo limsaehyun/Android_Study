ee081b595f5f186b2aff5378474d0e40803c3475
saehyun <sh00710000@gmail.com>
2021-10-11 01:18:10 +0900

    Feat :: Combine Method

---
+++ KotlinApp/RatioCalculator/app/src/main/java/com/saehyun/ratiocalculator/controller/CalculationActivity.kt
@@ -0,0 +1,57 @@
+package com.saehyun.ratiocalculator.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.saehyun.ratiocalculator.databinding.ActivityCalculationBinding
+import com.saehyun.ratiocalculator.model.PhoneSize
+import kotlin.math.*
+
+class CalculationActivity : AppCompatActivity() {
+    private val TAG = "CalculationActivity"
+
+    private lateinit var Phone: String
+
+    private var VerticalSize = 0
+    private var HorizontalSize = 0
+
+    private var phoneSize: PhoneSize = PhoneSize()
+
+    private lateinit var binding: ActivityCalculationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCalculationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Phone = intent.getStringExtra("select").toString()
+
+        getSize()
+
+        binding.tvCheckHorizontal.setOnClickListener {
+            if(binding.etDP.text.isNotEmpty()) {
+                Log.d(TAG, "onCreate: " + HorizontalSize + " " +phoneSize.GalaxyS9Horizontal)
+                val RESULT = binding.etDP.text.toString().toDouble() / HorizontalSize.toDouble()
+                Log.d(TAG, "onCreate: " + RESULT)
+                binding.tvResult.setText("${round(RESULT*100)/100}")
+            }
+        }
+
+        binding.tvCheckVertical.setOnClickListener {
+            if(binding.etDP.text.isNotEmpty()) {
+                val RESULT = binding.etDP.text.toString().toDouble() / VerticalSize.toDouble()
+                binding.tvResult.setText("${round(RESULT*100)/100}")
+            }
+        }
+    }
+
+    private fun getSize() {
+        when(Phone) {
+            "Galaxy S9" -> {
+                VerticalSize = phoneSize.GalaxtS9Vertical
+                HorizontalSize = phoneSize.GalaxyS9Horizontal
+            }
+        }
+    }
+
+}
