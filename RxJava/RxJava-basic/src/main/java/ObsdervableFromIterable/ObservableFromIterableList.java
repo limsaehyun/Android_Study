e0bf6801d30769092d8fd8eaaa0caf9c86648a48
limsaehyun <sh00710000@gmail.com>
2021-10-19 16:08:56 +0900

    Feat :: fromIterable() LIST 예제

---
+++ RxJava/RxJava-basic/src/main/java/ObsdervableFromIterable/ObservableFromIterableList.java
@@ -0,0 +1,20 @@
+package ObsdervableFromIterable;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObservableFromIterableList {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("임세현");
+        names.add("유현명");
+        names.add("이준서");
+
+        Observable<String> source = Observable.fromIterable(names);
+
+        source.subscribe(data -> System.out.println("Result : " + data));
+
+    }
+}
