0d4de7c63cbd919b2b28a21c4bdcf572a9547ee4
limsaehyun <sh00710000@gmail.com>
2021-10-19 15:24:45 +0900

    Feat :: fromArray() 예제

---
+++ RxJava/RxJava-basic/src/main/java/ObservableFromArray.java
@@ -0,0 +1,9 @@
+import io.reactivex.rxjava3.core.Observable;
+
+public class ObservableFromArray {
+    public static void main(String[] args) {
+        Integer[] arr = {100, 200 ,300};
+        Observable<Integer> source = Observable.fromArray(arr);
+        source.subscribe(data -> System.out.println("Result : " + data));
+    }
+}
