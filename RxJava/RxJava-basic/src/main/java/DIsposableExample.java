e3359fe0fb36aeee271b769dd49f59f5dc6f9124
limsaehyun <sh00710000@gmail.com>
2021-10-19 15:24:59 +0900

    Feat :: disposable 예제

---
+++ RxJava/RxJava-basic/src/main/java/DIsposableExample.java
@@ -0,0 +1,16 @@
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+public class DIsposableExample {
+    public static void main(String[] args) {
+        Observable<String> source = Observable.just("RED", "GREEN", "YELLOW");
+
+        Disposable d = source.subscribe(
+                v -> System.out.println("onNext() : value : " + v),
+                err -> System.out.println("onError : value : " + err.getMessage()),
+                () -> System.out.println("onComplete()")
+        );
+
+        System.out.println("isDisposed : " + d.isDisposed());
+    }
+}
