781e410447edca3a06df067559838c600cd7eb46
limsaehyun <sh00710000@gmail.com>
2021-10-19 16:08:38 +0900

    Feat :: fromIterable() SET 예제

---
+++ RxJava/RxJava-basic/src/main/java/ObsdervableFromIterable/ObservableFromIterableSet.java
@@ -0,0 +1,22 @@
+package ObsdervableFromIterable;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ObservableFromIterableSet {
+    public static void main(String[] args) {
+        Set<String> names = new HashSet<>();
+        names.add("임세현");
+        names.add("유현명");
+        names.add("이준서");
+
+        Observable<String> source = Observable.fromIterable(names);
+
+        source.subscribe(data -> System.out.println("Result : " + data));
+
+    }
+}
