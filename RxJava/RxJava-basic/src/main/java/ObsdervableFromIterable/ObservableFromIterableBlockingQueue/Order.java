894d3a6f6d49601a060023371ace41c4ace6da64
limsaehyun <sh00710000@gmail.com>
2021-10-19 16:09:07 +0900

    Feat :: Order

---
+++ RxJava/RxJava-basic/src/main/java/ObsdervableFromIterable/ObservableFromIterableBlockingQueue/Order.java
@@ -0,0 +1,18 @@
+package ObsdervableFromIterable.ObservableFromIterableBlockingQueue;
+
+public class Order {
+    private String mId;
+
+    public String getmId() {
+        return mId;
+    }
+
+    public Order(String mId) {
+        this.mId = mId;
+    }
+
+    @Override
+    public String toString() {
+        return "Order ID: " + mId;
+    }
+}
