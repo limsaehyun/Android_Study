7ec5ecefc9fe9f960567bc81066d624f9a2e7c30
limsaehyun <sh00710000@gmail.com>
2021-10-20 22:26:58 +0900

    Feat :: Single Sample

--- RxJava/RxJava-basic/src/main/java/Single/SingleExample.java
+++ RxJava/RxJava-basic/src/main/java/Single/SingleExample.java
@@ -1,5 +1,6 @@
 package Single;
 
+import Observable.ObsdervableFromIterable.ObservableFromIterableBlockingQueue.Order;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 
@@ -12,9 +13,26 @@ public class SingleExample {
                 .subscribe(System.out::println);
 
         // 2. single() 함수를 호출해 Single 객체 생성하기
-//        Observable.just("Hello Single")
-//                .single("default item")
-//                .subscribe(System.out::println);
+        Observable.just("Hello Single")
+                .single("default item")
+                .subscribe(System.out::println);
+
+        // 3. first() 함수를 호출해 Single 객체 생성하기
+        String[] colors = {"Red", "Blue", "Gold"};
+        Observable.fromArray(colors)
+                .first("default")
+                .subscribe(System.out::println);
+
+        // 4. empty Observable에서 Single 객체 생성하기
+        Observable.empty()
+                .single("default value")
+                .subscribe(System.out::println);
+
+        // 5. take() 함수에서 Single 객체 생성하기
+        Observable.just(new Order("ORD-1"), new Order("ORD-2"))
+                .take(1)
+                .single(new Order("default order"))
+                .subscribe(System.out::println);
 
     }
 }
