3b24e62d230d3e2d783a7a1b32832c7579513d2f
limsaehyun <sh00710000@gmail.com>
2021-10-20 22:15:35 +0900

    Feat :: class

--- RxJava/RxJava-basic/src/main/java/Single/SingleExample.java
+++ RxJava/RxJava-basic/src/main/java/Single/SingleExample.java
@@ -6,11 +6,15 @@ import io.reactivex.rxjava3.core.Single;
 public class SingleExample {
     public static void main(String[] args) {
 
-        // 1. 기존 Observable에서 Single 객체로 변환하기
-        Observable<String> source = Observable.just("Hello Single");
+        // 1. 기존 Observable에서 Single 객체로 변환하기기
+       Observable<String> source = Observable.just("Hello Single");
         Single.fromObservable(source)
                 .subscribe(System.out::println);
 
+        // 2. single() 함수를 호출해 Single 객체 생성하기
+//        Observable.just("Hello Single")
+//                .single("default item")
+//                .subscribe(System.out::println);
 
     }
 }
