e382cd8444dbc9404ca9ce686d3ec9b0bedda516
limsaehyun <sh00710000@gmail.com>
2021-10-20 19:51:11 +0900

    Feat :: fromFuture() Example

---
+++ RxJava/RxJava-basic/src/main/java/ObservableFromFuture.java
@@ -0,0 +1,18 @@
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ObservableFromFuture {
+    public static void main(String[] args) {
+        Future<String> future = Executors.newSingleThreadExecutor().submit(() -> {
+            // call() 메서드는 인자가 없으므로 () -> {} 형태로 나타낸다
+            Thread.sleep(1000);
+            return "Hello World!";
+        });
+
+        Observable<String> source = Observable.fromFuture(future);
+        source.subscribe(data -> System.out.println(data));
+    }
+}
