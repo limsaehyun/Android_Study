ad0726e7cb9a3d1b757f97f0db11abbcc0407536
limsaehyun <sh00710000@gmail.com>
2021-10-21 20:05:45 +0900

    Feat :: 구구단 예제 1

---
+++ RxJava/RxJava-Practice-Project/src/main/java/GuGudan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class GuGudan {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("input : ");
+        int dan = Integer.parseInt(in.nextLine());
+        for(int i = 1; i <= 9; i++) {
+            System.out.println(dan + " x " + i + " = " + dan*i);
+        }
+    }
+}
