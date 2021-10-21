2128d3a54c4374a374d73436904bd37665b2f623
limsaehyun <sh00710000@gmail.com>
2021-10-21 11:37:11 +0900

    Feat :: map() funtion Example

---
+++ RxJava/RxJava-basic/src/main/java/Operators/MapFuntionExample.java
@@ -0,0 +1,20 @@
+package Operators;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.functions.Function;
+import sun.rmi.runtime.Log;
+
+
+public class MapFuntionExample {
+
+    public static void main(String[] args) {
+        Function<String, String> getDiamond = ball -> ball + "◆";
+
+        String[] balls = {"1", "2", "3", "5"};
+        Observable<String> source = Observable.fromArray(balls)
+                .map(getDiamond);
+        source.subscribe(System.out::println);
+    }
+
+}
