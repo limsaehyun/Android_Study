c5cfb0a6f4cee2781c817e26e08a8f613c6b05b6
limsaehyun <sh00710000@gmail.com>
2021-10-21 20:06:05 +0900

    Feat :: Rx 구구단 예제 2

---
+++ RxJava/RxJava-Practice-Project/src/main/java/RxGuGudan2.java
@@ -0,0 +1,18 @@
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.functions.Function;
+
+import java.util.Scanner;
+
+public class RxGuGudan2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("input : ");
+        int dan = Integer.parseInt(in.nextLine());
+
+        Function<Integer, Observable<String>> gugudan = num ->
+                Observable.range(1, 9).map(row -> dan + " x " + row + " = " + dan*row);
+        Observable<String> source = Observable.just(dan).flatMap(gugudan);
+        source.subscribe(System.out::println);
+    }
+}
