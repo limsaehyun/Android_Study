e4d18f083c9a12d3ce93d994fcc429a72b75dbec
limsaehyun <sh00710000@gmail.com>
2021-10-23 20:24:39 +0900

    Feat :: IntervalRange Example

---
+++ RxJava/RxJava-basic/src/main/java/Operators/create/IntervalRangeExample.java
@@ -0,0 +1,19 @@
+package Operators.create;
+
+import Operators.common.CommonUtils;
+import Operators.common.Log;
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class IntervalRangeExample {
+    public static void main(String[] args) {
+        Observable<Long> source = Observable.intervalRange(1,
+                5,
+                100L,
+                100L,
+                TimeUnit.MILLISECONDS);
+        source.subscribe(Log::it);
+        CommonUtils.sleep(1000);
+    }
+}
