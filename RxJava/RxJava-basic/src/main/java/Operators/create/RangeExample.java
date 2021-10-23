a630b9c16ba61351cf22a4b925a93a5e9ce46fe8
limsaehyun <sh00710000@gmail.com>
2021-10-23 20:25:03 +0900

    Feat :: range Example

---
+++ RxJava/RxJava-basic/src/main/java/Operators/create/RangeExample.java
@@ -0,0 +1,12 @@
+package Operators.create;
+
+import Operators.common.Log;
+import io.reactivex.rxjava3.core.Observable;
+
+public class RangeExample {
+    public static void main(String[] args) {
+        Observable<Integer> source = Observable.range(1, 10)
+                .filter(number -> number % 2 == 0);
+        source.subscribe(Log::it);
+    }
+}
