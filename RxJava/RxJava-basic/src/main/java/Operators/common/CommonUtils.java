fdcc85e6818d210e518ddc83103dc3e7a5fbfa74
limsaehyun <sh00710000@gmail.com>
2021-10-23 20:23:46 +0900

    Feat :: CommonUtils

---
+++ RxJava/RxJava-basic/src/main/java/Operators/common/CommonUtils.java
@@ -0,0 +1,17 @@
+package Operators.common;
+
+public class CommonUtils {
+    public static long startTime;
+
+    public static void exampleStart() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
