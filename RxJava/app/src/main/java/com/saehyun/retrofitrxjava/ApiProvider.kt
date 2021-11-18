14f27bee19d96d66f91d7154c0911ad2e29eff15
limsaehyun <sh00710000@gmail.com>
2021-11-18 13:08:42 +0900

    Feat :: RxJava Retrofit2 Example

---
+++ RxJava/app/src/main/java/com/saehyun/retrofitrxjava/ApiProvider.kt
@@ -0,0 +1,27 @@
+package com.saehyun.retrofitrxjava
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object ApiProvider {
+    val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    private val client = OkHttpClient
+        .Builder()
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+        .create(Server::class.java)
+
+    fun buildService(): Server {
+        return retrofit
+    }
+}
