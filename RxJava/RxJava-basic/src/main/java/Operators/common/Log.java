c68a1ea50eadc85137b8a4fb95b2bea5333a16ca
limsaehyun <sh00710000@gmail.com>
2021-11-20 16:53:10 +0900

    Feat :: Log

--- RxJava/RxJava-basic/src/main/java/Operators/common/Log.java
+++
@@ -1,8 +0,0 @@
-package Operators.common;
-
-public class Log {
-    public static void it(Object object) {
-        long time = System.currentTimeMillis() - CommonUtils.startTime;
-        System.out.println("Thread - 1" + time  + " | value = " + object);
-    }
-}
