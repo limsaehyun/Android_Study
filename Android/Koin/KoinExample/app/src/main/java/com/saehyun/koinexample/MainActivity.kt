7258e869caf800e13a856228e387a054dbd4b935
limsaehyun <sh00710000@gmail.com>
2021-11-22 18:49:31 +0900

    Feat :: named사용

--- Android/Koin/KoinExample/app/src/main/java/com/saehyun/koinexample/MainActivity.kt
+++ Android/Koin/KoinExample/app/src/main/java/com/saehyun/koinexample/MainActivity.kt
@@ -8,6 +8,7 @@ import com.saehyun.koinexample.objet.Teacher
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 class MainActivity : AppCompatActivity() {
@@ -19,16 +20,21 @@ class MainActivity : AppCompatActivity() {
             androidContext(this@MainActivity)
             modules(
                 module {
-                    single { Teacher("teacher-cho")
-                    factory { Student("student-kim") }}
+                    single(named("cho")) { Student("student-cho") }
+                    single(named("kim")) { Student("student-kim") }
+                    factory { Teacher("teacher-cho") }
                 }
             )
         }
 
         val teacher : Teacher by inject() // 의존성 주입
-        val student : Student by inject()
+
+        val studentCho : Student by inject(named("cho"))
+        val studentKim : Student by inject(named("kim"))
 
         findViewById<TextView>(R.id.textViewTeacher).text = teacher.name
-        findViewById<TextView>(R.id.textViewStudent).text = student.name
+
+        findViewById<TextView>(R.id.textViewStudent).text = studentCho.name
+        findViewById<TextView>(R.id.textViewStudent).text = studentKim.name
     }
 }
