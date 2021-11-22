151d8629782626fa4aadd1c2a4220a41abe3977a
limsaehyun <sh00710000@gmail.com>
2021-11-22 17:27:21 +0900

    Feat :: Container 만들기

--- Android/Koin/KoinExample/app/src/main/java/com/saehyun/koinexample/MainActivity.kt
+++ Android/Koin/KoinExample/app/src/main/java/com/saehyun/koinexample/MainActivity.kt
@@ -2,10 +2,31 @@ package com.saehyun.koinexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.saehyun.koinexample.objet.Student
+import com.saehyun.koinexample.objet.Teacher
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startKoin {
+            androidContext(this@MainActivity)
+            modules(
+                module {
+                    single { Teacher("teacher-cho")
+                    factory { Student("student-kim") }}
+                }
+            )
+        }
+
+        val teacher : Teacher by inject() // 의존성 주입
+        val student : Student by inject()
+
+        findViewById<Teacher>(R.id.text)
     }
 }
