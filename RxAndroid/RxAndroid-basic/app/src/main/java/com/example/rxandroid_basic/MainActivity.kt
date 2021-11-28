0ae93a5541ebb689edf456697b2cba683eaf04c4
saehyun <sh00710000@gmail.com>
2021-11-29 01:17:14 +0900

    Init

---
+++ RxAndroid/RxAndroid-basic/app/src/main/java/com/example/rxandroid_basic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rxandroid_basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
