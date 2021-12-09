f8a687e0305a4796af6482cd04187fe666fbfec6
limsaehyun <sh00710000@gmail.com>
2021-12-09 11:53:55 +0900

    Feat :: Dagger사용하기

--- Android/Dagger2/Dagger2Example/app/src/main/java/com/saehyun/dagger2example/MainActivity.kt
+++ Android/Dagger2/Dagger2Example/app/src/main/java/com/saehyun/dagger2example/MainActivity.kt
@@ -2,10 +2,31 @@ package com.saehyun.dagger2example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.saehyun.dagger2example.di.DaggerPetComponent
+import com.saehyun.dagger2example.module.CatModule
+import com.saehyun.dagger2example.module.DogModule
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var cat: Cat
+
+    @Inject
+    lateinit var dog: Dog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DaggerPetComponent.builder()
+            .catModule(CatModule)
+            .dogModule(DogModule)
+            .build()
+            .inject(this)
+
+        Log.d("Main", "onCreate: ${dog.getDogName() + cat.getCatName()}")
     }
 }
