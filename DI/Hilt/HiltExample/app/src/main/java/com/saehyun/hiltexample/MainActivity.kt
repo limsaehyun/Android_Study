3d62c1acc8eca79483975e09eb8dfc4bccfcf7e9
limsaehyun <sh00710000@gmail.com>
2021-12-22 09:38:41 +0900

    Feat :: vm 실행

--- DI/Hilt/HiltExample/app/src/main/java/com/saehyun/hiltexample/MainActivity.kt
+++ DI/Hilt/HiltExample/app/src/main/java/com/saehyun/hiltexample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.saehyun.hiltexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var vm : MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        vm.getPost()
+
+        vm.success.observe(this, {
+            Toast.makeText(applicationContext, "${it}", Toast.LENGTH_SHORT).show()
+        })
+        vm.error.observe(this, {
+            Toast.makeText(applicationContext, "${it}", Toast.LENGTH_SHORT).show()
+        })
     }
 }
