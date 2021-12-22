04b07169e8c93265ad6740cb060869d62f865c7f
limsaehyun <sh00710000@gmail.com>
2021-12-22 09:38:33 +0900

    Feat :: RepositoryModule

---
+++ DI/Hilt/HiltExample/app/src/main/java/com/saehyun/hiltexample/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.saehyun.hiltexample.di
+
+import com.saehyun.hiltexample.MainRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(): MainRepository = MainRepository()
+}
