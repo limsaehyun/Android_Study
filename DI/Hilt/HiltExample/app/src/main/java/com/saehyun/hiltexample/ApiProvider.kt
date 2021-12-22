af45d1e6c12cae3eb429958fed3559988d7e7368
limsaehyun <sh00710000@gmail.com>
2021-12-22 10:23:42 +0900

    Fix :: BaseURL

--- DI/Hilt/HiltExample/app/src/main/java/com/saehyun/hiltexample/ApiProvider.kt
+++ DI/Hilt/HiltExample/app/src/main/java/com/saehyun/hiltexample/ApiProvider.kt
@@ -5,7 +5,7 @@ import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 private val retrofit: Retrofit = Retrofit.Builder()
-    .baseUrl("URL")
+    .baseUrl("https://jsonplaceholder.typicode.com/")
     .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
     .addConverterFactory(GsonConverterFactory.create())
     .build()
