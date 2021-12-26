7c51866a7b6d4572db12aba11c3d6f61d15e83ad
임세현 <sh00710000@gmail.com>
2021-12-26 23:01:13 +0900

    Feat :: MainViewModel 구현

--- ArchitecturePattern/MVVM/MVVMCalculator/app/src/main/java/com/saehyun/mvvmcalculator/MainViewModel.kt
+++ ArchitecturePattern/MVVM/MVVMCalculator/app/src/main/java/com/saehyun/mvvmcalculator/MainViewModel.kt
@@ -1,8 +1,14 @@
 package com.saehyun.mvvmcalculator
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel() : ViewModel() {
-    val toast
+    val toastmessage: MutableLiveData<Int> = MutableLiveData()
+
+    fun plus(mainData: MainData) {
+        val sum: Int = mainData.num1 + mainData.num2
+        toastmessage.value = sum
+    }
 
 }
