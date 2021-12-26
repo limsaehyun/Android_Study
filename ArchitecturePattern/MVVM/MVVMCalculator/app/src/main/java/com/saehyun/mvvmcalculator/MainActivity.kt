6a463306807afba8f250440c12b9bfd28bb429f7
임세현 <sh00710000@gmail.com>
2021-12-26 22:58:35 +0900

    Feat :: 계산 기능구현

--- ArchitecturePattern/MVVM/MVVMCalculator/app/src/main/java/com/saehyun/mvvmcalculator/MainActivity.kt
+++ ArchitecturePattern/MVVM/MVVMCalculator/app/src/main/java/com/saehyun/mvvmcalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.saehyun.mvvmcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.saehyun.mvvmcalculator.databinding.ActivityMainBinding
 
@@ -9,18 +10,29 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var vm = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        observeEvent()
+
         binding.run {
             btn.setOnClickListener {
-                val num1 = et1.text.toString()
-                val num2 = et2.text.toString()
-                if(num1.isNotEmpty() && num2.isNotEmpty()) {
-
+                val num1 = et1.text.toString().toInt()
+                val num2 = et2.text.toString().toInt()
+                if(num1 != 0 && num2 != 0) {
+                    vm.plus(MainData(num1, num2))
                 }
             }
         }
+    }
+
+    private fun observeEvent() {
+        vm.toastmessage.observe(this, {
+            binding.result.text = it.toString()
+            Toast.makeText(applicationContext, "${it}", Toast.LENGTH_SHORT).show()
+        })
     }
 }
