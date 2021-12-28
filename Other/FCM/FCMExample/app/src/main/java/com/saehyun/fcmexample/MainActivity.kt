f719658fdaba875a9db9e01a3a495a58efb9e3a3
임세현 <sh00710000@gmail.com>
2021-12-28 22:17:28 +0900

    Init

---
+++ Other/FCM/FCMExample/app/src/main/java/com/saehyun/fcmexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.saehyun.fcmexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
