7969de7c233f2bf7108d557764ddfa19dd95d5b7
임세현 <sh00710000@gmail.com>
2022-01-05 19:26:52 +0900

    Init & gradle

---
+++ KotlinApp/KakaoAuthExample/app/src/main/java/com/saehyun/kakaoauthexample/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.saehyun.kakaoauthexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import java.security.NoSuchAlgorithmException
+
+import java.security.MessageDigest
+
+import android.content.pm.PackageManager
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager.NameNotFoundException
+import android.util.Base64
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getHashKey()
+
+    }
+
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
+    }
+
+}
