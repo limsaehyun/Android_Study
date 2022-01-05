ae0dcf5a350d105fee7f00cd28558ba3fdbff288
임세현 <sh00710000@gmail.com>
2022-01-05 09:32:42 +0900

    Feat :: 여러 텍스트 출력해보기

---
+++ JetPack/Compose/JetpackComposeExample/app/src/main/java/com/saehyun/jetpackcomposeexample/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.saehyun.jetpackcomposeexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(Message("Colleague", "LookAt Me"))
+}
