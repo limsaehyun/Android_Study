4d7905ed80c098ef42658487130961afa0b538a7
임세현 <sh00710000@gmail.com>
2022-01-05 19:26:46 +0900

    Feat :: 도형 만들기

--- JetPack/Compose/JetpackComposeExample/app/src/main/java/com/saehyun/jetpackcomposeexample/MainActivity.kt
+++ JetPack/Compose/JetpackComposeExample/app/src/main/java/com/saehyun/jetpackcomposeexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.saehyun.jetpackcomposeexample
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
@@ -7,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -26,10 +28,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
 data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(msg: Message) {
+fun MessageCard(msg: Message) {ㄷ
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.icon),
@@ -47,16 +50,26 @@ fun MessageCard(msg: Message) {
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = msg.body,
-                style = MaterialTheme.typography.body2
-            )
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
     MaterialTheme {
