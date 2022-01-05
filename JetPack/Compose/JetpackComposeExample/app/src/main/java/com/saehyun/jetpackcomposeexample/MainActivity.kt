8ee9b3f197d135bcd9d702123582442da18c007b
임세현 <sh00710000@gmail.com>
2022-01-05 10:01:32 +0900

    Feat :: 수정자 사용하기

--- JetPack/Compose/JetpackComposeExample/app/src/main/java/com/saehyun/jetpackcomposeexample/MainActivity.kt
+++ JetPack/Compose/JetpackComposeExample/app/src/main/java/com/saehyun/jetpackcomposeexample/MainActivity.kt
@@ -3,9 +3,16 @@ package com.saehyun.jetpackcomposeexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +27,23 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.icon),
+            contentDescription = "picture description",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
