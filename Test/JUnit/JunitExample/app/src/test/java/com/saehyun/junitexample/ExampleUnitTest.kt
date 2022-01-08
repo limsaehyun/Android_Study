783cdb658f2205e7e3adf11d64a58f570fe7c315
임세현 <sh00710000@gmail.com>
2022-01-08 13:26:22 +0900

    Test :: addValue

--- Test/JUnit/JunitExample/app/src/test/java/com/saehyun/junitexample/ExampleUnitTest.kt
+++ Test/JUnit/JunitExample/app/src/test/java/com/saehyun/junitexample/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.saehyun.junitexample
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,17 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    private lateinit var calculator: Calculator
+
+    @Before
+    fun init() {
+        calculator = Calculator()
     }
+
+    @Test
+    fun addValue_test() {
+        assertEquals(52, calculator.addValue(50, 2))
+        assertEquals(52, calculator.addValue(50, 1))
+    }
+
 }
