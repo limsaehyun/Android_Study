5cf14b23593be4f9c41fab7186368fd693f53679
임세현 <sh00710000@gmail.com>
2022-01-12 14:10:41 +0900

    Feat :: mutableStateOf

--- JetPack/Compose/ComposeExample2/app/src/main/java/com/saehyun/composeexample2/MainActivity.kt
+++ JetPack/Compose/ComposeExample2/app/src/main/java/com/saehyun/composeexample2/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.*
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,21 +33,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column {
+    Column(
+        modifier = Modifier.padding(vertical = 4.dp)
+    ) {
         for(name in names) {
-            Surface(color = MaterialTheme.colors.background) {
-                Greeting(name = name)
-            }
+            Greeting(name = name)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(text = "Hello,")
-            Text(text = name)
+    val expanded = remember { mutableStateOf(false) }
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if(expanded.value) "Show less" else "Show more")
+            }
         }
     }
 }
