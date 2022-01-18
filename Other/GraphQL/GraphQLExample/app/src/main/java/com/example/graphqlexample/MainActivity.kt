e761becc3ac867171c21421f9fb799aa560b8d17
임세현 <sh00710000@gmail.com>
2022-01-18 21:52:06 +0900

    Feat :: CallBack

--- Other/GraphQL/GraphQLExample/app/src/main/java/com/example/graphqlexample/MainActivity.kt
+++ Other/GraphQL/GraphQLExample/app/src/main/java/com/example/graphqlexample/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.graphqlexample
 
+import FeedResultQuery
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apolloClient = ApolloClient.builder()
+            .serverUrl("https://rickandmortyapi.com/graphql/")
+            .build()
+
+        apolloClient.query(FeedResultQuery())
+            .enqueue(object : ApolloCall.Callback<FeedResultQuery.Data>() {
+                override fun onResponse(response: Response<FeedResultQuery.Data>) {
+                    Log.d(TAG, "onResponse: ${response.data?.characters?.results}")
+                }
+
+                override fun onFailure(e: ApolloException) {
+                    Log.d(TAG, "onFailure: ")
+                }
+            })
     }
 }
