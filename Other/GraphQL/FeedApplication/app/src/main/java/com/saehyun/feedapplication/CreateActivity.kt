2b3033fa0291e0f8fcfe0cff0180b9ca61beaa45
임세현 <sh00710000@gmail.com>
2022-01-20 09:44:06 +0900

    📑 :: create product activity

---
+++ Other/GraphQL/FeedApplication/app/src/main/java/com/saehyun/feedapplication/CreateActivity.kt
@@ -0,0 +1,51 @@
+package com.saehyun.feedapplication
+
+import CreateMutation
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.saehyun.feedapplication.databinding.ActivityCreateBinding
+
+class CreateActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCreateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCreate.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val price = binding.etPrice.text.toString().toInt()
+
+            create(title, price)
+        }
+    }
+
+    private fun create(name: String, price: Int) {
+        ApiProvider.apolloClient.mutate(CreateMutation(name, price))
+            .enqueue(object : ApolloCall.Callback<CreateMutation.Data>() {
+                override fun onResponse(response: Response<CreateMutation.Data>) {
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "success!", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+
+                }
+
+                override fun onFailure(e: ApolloException) {
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "error!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+    }
+
+}
