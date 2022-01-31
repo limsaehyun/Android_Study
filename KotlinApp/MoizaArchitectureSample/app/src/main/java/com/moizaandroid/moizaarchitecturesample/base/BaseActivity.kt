52b8e9383dad9715ec726cdac7c1ba9483aed552
saehyun <sh00710000@gmail.com>
2022-01-31 23:49:08 +0900

    📑 :: Base

---
+++ KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/base/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.moizaandroid.moizaarchitecturesample.base
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B: ViewDataBinding> (
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+        observeEvent()
+    }
+
+    fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    abstract fun observeEvent()
+}
