c9697337b9ff5616301c5cbaed2e7a9099f7462f
saehyun <sh00710000@gmail.com>
2022-02-01 22:46:08 +0900

    ⚡️ :: retrofit module

---
+++ KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/di/module/RetrofitModule.kt
@@ -0,0 +1,49 @@
+package com.moizaandroid.moizaarchitecturesample.di.module
+
+import android.util.Log
+import com.moizaandroid.moizaarchitecturesample.data.remote.network.PostApi
+import com.moizaandroid.moizaarchitecturesample.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor { message -> Log.v("HTTP", message) }
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    fun providePostApi(
+        retrofit: Retrofit
+    ): PostApi =
+        retrofit.create(PostApi::class.java)
+}
