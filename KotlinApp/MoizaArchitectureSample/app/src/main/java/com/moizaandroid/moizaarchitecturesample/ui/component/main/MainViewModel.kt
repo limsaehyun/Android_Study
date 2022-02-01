8ba91e7f7b1cd28095386f1dae86ec41bc0721de
saehyun <sh00710000@gmail.com>
2022-02-01 22:46:49 +0900

    ⚡️ :: main viewmodel

---
+++ KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/ui/component/main/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.moizaandroid.moizaarchitecturesample.ui.component.main
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.moizaandroid.moizaarchitecturesample.data.remote.network.ResultWrapper
+import com.moizaandroid.moizaarchitecturesample.data.repository.PostRepository
+import com.moizaandroid.moizaarchitecturesample.utils.ApiException
+import com.moizaandroid.moizaarchitecturesample.utils.NoInternetException
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import java.lang.RuntimeException
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val mainRepository: PostRepository
+) : ViewModel() {
+
+    val postData = MutableLiveData<String>()
+    val errorToast = MutableLiveData<String>()
+
+    fun fetchPost(postId: Int) {
+        viewModelScope.launch {
+            val fetchPost = mainRepository.fetchPost(postId)
+            when(fetchPost) {
+                is ResultWrapper.NetworkError -> {
+                    Log.d("Main", "fetchPost: n")
+                }
+                is ResultWrapper.GenericError -> {
+                    Log.d("Main", "fetchPost: e")
+                }
+                is ResultWrapper.Success -> {
+                    Log.d("Main", "fetchPost: s")
+                }
+            }
+        }
+    }
+
+}
