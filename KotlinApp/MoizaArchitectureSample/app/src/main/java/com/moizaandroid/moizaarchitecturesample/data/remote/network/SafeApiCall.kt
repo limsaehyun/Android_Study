4a13532e3badf4114b1bd011f34e03ecb500ed46
saehyun <sh00710000@gmail.com>
2022-02-01 22:44:50 +0900

    📑 :: safe api request

---
+++ KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/data/remote/network/SafeApiCall.kt
@@ -0,0 +1,36 @@
+package com.moizaandroid.moizaarchitecturesample.data.remote.network
+
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+suspend fun <T> safeApiCall(apiCall: suspend () -> Response<T>): ResultWrapper<T?> {
+    return try {
+        val response = apiCall.invoke()
+        return ResultWrapper.Success(response.body())
+    }catch (throwable: Throwable){
+        when(throwable){
+            is IOException -> ResultWrapper.NetworkError
+            is HttpException -> {
+                val code = throwable.code()
+                val errorResponse = convertErrorBody(throwable)
+                ResultWrapper.GenericError(code, errorResponse)
+            }
+            else -> ResultWrapper.GenericError(null, null)
+        }
+    }
+}
+
+private fun convertErrorBody(throwable: HttpException) : ErrorResponse?{
+    return try {
+        throwable.response()?.errorBody()?.source()?.let {
+            val adap = Moshi.Builder().build().adapter(ErrorResponse::class.java)
+            adap.fromJson(it)
+        }
+    }catch (e: Exception){
+        null
+    }
+}
