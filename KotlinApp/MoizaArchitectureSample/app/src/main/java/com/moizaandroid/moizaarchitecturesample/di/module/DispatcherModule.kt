f0ffbd99639144cb46c793a41ac9acb95bbf094c
saehyun <sh00710000@gmail.com>
2022-02-01 22:45:33 +0900

    ⚡️ :: dispatcher module

---
+++ KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/di/module/DispatcherModule.kt
@@ -0,0 +1,22 @@
+package com.moizaandroid.moizaarchitecturesample.di.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
