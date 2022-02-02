bef5fde48eca694423f01d63923c6f566e304b13
saehyun <sh00710000@gmail.com>
2022-02-02 23:18:13 +0900

    ⚡️ :: PostRoomEntity

---
+++ KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/data/local/entity/PostRoomEntity.kt
@@ -0,0 +1,8 @@
+package com.moizaandroid.moizaarchitecturesample.data.local.entity
+
+data class PostRoomEntity(
+    val body: String,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
