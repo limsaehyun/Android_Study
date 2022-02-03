9405ae804bbd0f5c9019802a11b044af7e76a4f5
saehyun <sh00710000@gmail.com>
2022-02-03 15:40:06 +0900

    ⚡️ :: post room entity toResponse

--- KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/data/local/entity/PostRoomEntity.kt
+++ KotlinApp/MoizaArchitectureSample/app/src/main/java/com/moizaandroid/moizaarchitecturesample/data/local/entity/PostRoomEntity.kt
@@ -1,8 +1,20 @@
 package com.moizaandroid.moizaarchitecturesample.data.local.entity
 
+import androidx.room.Entity
+import com.moizaandroid.moizaarchitecturesample.data.remote.dto.response.PostResponse
+
+@Entity (tableName = "postEntity")
 data class PostRoomEntity(
     val body: String,
     val id: Int,
     val title: String,
     val userId: Int
 )
+
+fun PostRoomEntity.toResponse() =
+    PostResponse(
+        body = body,
+        id = id,
+        title = title,
+        userId = userId
+    )
