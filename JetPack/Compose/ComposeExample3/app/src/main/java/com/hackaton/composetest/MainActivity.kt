b6015f367aa916ab72f6e099cd28b3a997274733
임세현 <sh00710000@gmail.com>
2022-03-14 16:58:25 +0900

    ⚡️ :: photographer card

--- JetPack/Compose/ComposeExample3/app/src/main/java/com/hackaton/composetest/MainActivity.kt
+++ JetPack/Compose/ComposeExample3/app/src/main/java/com/hackaton/composetest/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.hackaton.composetest
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
--- JetPack/Compose/ComposeExample3/app/src/main/java/com/hackaton/composetest/ProfileActivity.kt
+++ JetPack/Compose/ComposeExample3/app/src/main/java/com/hackaton/composetest/ProfileActivity.kt
@@ -3,11 +3,21 @@ package com.hackaton.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hackaton.composetest.ui.theme.ComposeTestTheme
 
 class ProfileActivity : ComponentActivity() {
@@ -15,9 +25,8 @@ class ProfileActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerCard()
                 }
             }
         }
@@ -25,14 +34,44 @@ class ProfileActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard() {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.onSurface)
+            .clickable(onClick = {})
+            .padding(16.dp)
+    ) {
+
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.surface.copy(alpha = 0.2f)
+        ) {
+
+        }
+
+        Column(
+            Modifier
+                .padding(start = 8.dp)
+                .align(CenterVertically)
+        ) {
+            Text(text = "Lim Sae Hyun", fontWeight = FontWeight.Bold)
+
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTestTheme {
-        Greeting("Android")
+        Surface(color = MaterialTheme.colors.background) {
+            PhotographerCard()
+        }
     }
 }
