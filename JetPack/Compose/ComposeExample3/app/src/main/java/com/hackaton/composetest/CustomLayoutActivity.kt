d74878c5d93b43cb06c2e9776988f21e8625ddfc
임세현 <sh00710000@gmail.com>
2022-03-18 15:16:17 +0900

    ⚡️ :: custom layout activity

--- JetPack/Compose/ComposeExample3/app/src/main/java/com/hackaton/composetest/CustomLayoutActivity.kt
+++ JetPack/Compose/ComposeExample3/app/src/main/java/com/hackaton/composetest/CustomLayoutActivity.kt
@@ -3,11 +3,21 @@ package com.hackaton.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.hackaton.composetest.ui.theme.ComposeTestTheme
 
 class CustomLayoutActivity : ComponentActivity() {
@@ -15,24 +25,92 @@ class CustomLayoutActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Row {
+                        TextWithPaddingToBaselinePreview()
+                        TextWithNormalPaddingPreview()
+                        CallingComposable()
+                    }
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TextWithPaddingToBaselinePreview() {
+    Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
 }
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    Text("Hi there!", Modifier.padding(top = 32.dp))
+}
+
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = layout { measurable, constraints ->
+    // 파라미터에 측정할 수 있는 요소 : measurable
+    // Composable에 수식된 제약조건 constraints
+
+    val placeable = measurable.measure(constraints)
+
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+
+        placeable.placeRelative(0, placeableY)
+    }
+}
+
+@Composable
+fun MyBasicColumn(
+    modifier: Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map {
+            it.measure(constraints)
+        }
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var yPosition = 0
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun CallingComposable(modifier: Modifier = Modifier) {
+    MyBasicColumn(modifier.padding(8.dp)) {
+        Text("MyBasicColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview7() {
     ComposeTestTheme {
-        Greeting("Android")
+        Row {
+            TextWithPaddingToBaselinePreview()
+            TextWithNormalPaddingPreview()
+            CallingComposable()
+        }
     }
 }
