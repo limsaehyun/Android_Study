e1e6ff7254f6f8fa66c6431361957d2afc0d3088
임세현 <sh00710000@gmail.com>
2022-03-18 23:42:38 +0900

    📑 :: compose UI

---
+++ JetPack/Compose/CloneDesign/app/src/main/java/com/saehyun/clonedesign/ui/main/MainScreenUI.kt
@@ -0,0 +1,17 @@
+package com.saehyun.clonedesign.ui
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.saehyun.clonedesign.bottomNav.BottomNavigationBar
+import com.saehyun.clonedesign.bottomNav.NavigationGraph
+
+@Composable
+fun MainScreen(){
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) {
+        NavigationGraph(navController)
+    }
+}
---
+++ JetPack/Compose/CloneDesign/app/src/main/java/com/saehyun/clonedesign/ui/notification/NotificationUI.kt
@@ -0,0 +1,43 @@
+package com.saehyun.clonedesign.ui.notification
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.saehyun.clonedesign.R
+
+@Composable
+fun NotificationScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.white))
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Notification View",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewNotificationScreen() {
+    NotificationScreen()
+}
