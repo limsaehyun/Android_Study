1913af2dbfa18fdc00cf36e783af4ef102238556
임세현 <sh00710000@gmail.com>
2022-03-20 17:34:22 +0900

    📑 :: state activity & img

---
+++ JetPack/Compose/ComposeExample3/app/src/main/java/com/hackaton/composetest/ui/activity/StateActivity.kt
@@ -0,0 +1,12 @@
+package com.hackaton.composetest.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hackaton.composetest.R
+
+class StateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_state)
+    }
+}
