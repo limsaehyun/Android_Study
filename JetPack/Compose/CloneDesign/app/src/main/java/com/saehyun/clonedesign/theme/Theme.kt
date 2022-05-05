29514b77536dcb3dea377fde8ddde6ffad982d3c
임세현 <sh00710000@gmail.com>
2022-05-05 20:11:20 +0900

    ⚰️ :: 주석 제거

--- JetPack/Compose/CloneDesign/app/src/main/java/com/saehyun/clonedesign/theme/Theme.kt
+++ JetPack/Compose/CloneDesign/app/src/main/java/com/saehyun/clonedesign/theme/Theme.kt
@@ -15,15 +15,6 @@ private val DarkColorPalette = darkColors(
 private val LightColorPalette = lightColors(
     primary = Color.White,
     secondary = Mint
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
