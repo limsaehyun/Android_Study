0657f49a07f6803e388043cff74ff3718b086755
임세현 <sh00710000@gmail.com>
2022-05-07 23:52:23 +0900

    ⚡️ :: notification title

--- JetPack/Compose/CloneDesign/app/src/main/java/com/saehyun/clonedesign/ui/notification/NotificationUI.kt
+++ JetPack/Compose/CloneDesign/app/src/main/java/com/saehyun/clonedesign/ui/notification/NotificationUI.kt
@@ -3,6 +3,7 @@ package com.saehyun.clonedesign.ui.notification
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,8 +14,10 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.saehyun.clonedesign.R
+import com.saehyun.clonedesign.theme.Mint
 
 @Composable
 fun NotificationScreen() {
@@ -22,15 +25,13 @@ fun NotificationScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.white))
-            .wrapContentSize(Alignment.Center)
     ) {
         Text(
-            text = "Notification View",
+            text = "대마고\n기숙사\n안내사항",
             fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
+            color = Mint,
+            fontSize = 25.sp,
+            modifier = Modifier.padding(start = 24.dp, top = 24.dp)
         )
     }
 
